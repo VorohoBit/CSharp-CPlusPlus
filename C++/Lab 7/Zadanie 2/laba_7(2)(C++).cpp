d4054e0d9ab4f5186b60d3e16283854b17fef094
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 22:23:50 +0300

    Lab 7 Zadanie 2
    Выполнить задание номер 3 лабораторной работы No6 («Разработка и отладка алгоритмов и
    программ по обработке строк символов»), оформив каждый пункт задания в виде функции. Все
    необходимые данные для функций должны передаваться им в качестве параметров.
    Использование глобальных переменных в функциях не допускается.

---
+++ C++/Lab 7/Zadanie 2/laba_7(2)(C++).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <string>
+using namespace std;
+
+void resh1(string str);
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string str;
+    bool l = true;
+    while (l)
+    {
+        cout << "Введите строку:"; 
+        getline(cin, str);
+        resh1(str);
+        if (str.empty() == true)
+        {
+            l = false;
+        }
+    }
+    return 0;
+}
+void resh1(string str)
+{
+    for (int i = 0; i < str.size(); i++) //size() – возвращающает для строк кол-во символов
+        if ((i == 0 || str[i - 1] == ' ')) str[i] = str[i] - 32;
+        cout << "Полученная строка:" << str << endl;
+}
+
+
