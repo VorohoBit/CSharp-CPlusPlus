b1eca6e43048ced4dc6858f79b5255c19f88942a
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-20 00:24:14 +0300

    Lab 12
    Для класса В переопределить операции согласно варианту (см. вариант в
    таблице 3). В основной программе продемонстрировать использование
    переопределенных операций.
    
    true, false, |

---
+++ C#/Lab 12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C_sharp__laba12
+{
+    class MyClass
+    {
+        private int x, y;
+        public MyClass(int x, int y)
+        {
+            this.x = x; this.y = y;
+        }
+        public static bool operator true(MyClass v)
+        {
+            if ((v.x != 0) || (v.y != 0))  return true; 
+            else return false;
+        }
+        public static bool operator false(MyClass v)
+        {
+            if ((v.x == 0) && (v.y == 0)) return true;
+            else return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass(0, 0);
+            if (my) Console.WriteLine("True");
+            else Console.WriteLine("False");
+        }
+    }
+}
