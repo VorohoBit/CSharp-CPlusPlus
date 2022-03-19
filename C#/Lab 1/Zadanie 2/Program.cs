17f70330da42e7ce805120437ece4e117b4ae8f0
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 23:34:42 +0300

    Lab 2 Zadanie 2
    Составить процедуру для выполнения расчётов функции, значения задавать в диалоге с использованием метода Console.ReadLine()

---
+++ C#/Lab 1/Zadanie 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C_sharp__laba1_zadanie_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            Console.WriteLine("Введите X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = 1.3 * Math.Pow(Math.E, -x / 2)+Math.Abs(Math.Cos(((2*pi*x)/3)-1.4))-6/11;
+            Console.WriteLine("При X= {0} ; Y= {1}", x, y);
+        }
+    }
+}
