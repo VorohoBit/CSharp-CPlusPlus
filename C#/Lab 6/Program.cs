bc915e76f14df7c6524974d498bfe09dfb8d1709
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-20 00:11:18 +0300

    Lab 6
    Для заданного условия составить процедуру и придумать несколько наборов текстовых данных для откладки. Возможно использование как статических массивов, так и динамических. Ввод исходных данных осуществить из файла или с клавиатуры. 
    Написать процедуру пересылки двумерного массива А(5,6) в одномерный массив В(30) того же размера, по строкам с сохранением порядка следования элементов.

---
+++ C#/Lab 6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace C_sharp__laba6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите нижний диапазон:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите верхний диапазон:");
+            int l = Convert.ToInt32(Console.ReadLine());
+            int[,] Matrix = new int[5, 6];
+            Random rnd = new Random();
+            int z = 0;
+            int[] Massive = new int[30];
+            for (int i=0;i<5;i++)
+            {
+                Console.WriteLine("\n");
+                for (int j=0;j<6;j++)
+                {
+                    Matrix[i,j] = rnd.Next(m,l);
+                    Console.Write("{0, 5}",Matrix[i,j]);
+                    Massive[z] = Matrix[i, j];
+                    z++;
+                }
+            }
+            Console.WriteLine("\n\nОдномерный массив того же размера,по строкам с сохранением порядка следования элементов:");
+            for (int i=0;i<30;i++)
+            {
+                Console.WriteLine();
+                Console.Write("{0, 5}", Massive[i]);
+            }
+        }
+    }
+}
