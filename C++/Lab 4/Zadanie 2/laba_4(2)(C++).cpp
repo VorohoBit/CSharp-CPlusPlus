ef11cabed2f06fa588bb165682a2adb8b17564cb
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 22:12:56 +0300

    Lab 4 Zadanie 2
    Осуществить циклический сдвиг элементов прямоугольной матрицы на n элементов вправо или
    вниз (в зависимости от введенного режима), n может быть больше количества элементов в
    строке или столбце.

---
+++ C++/Lab 4/Zadanie 2/laba_4(2)(C++).cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <locale.h>
+#define size 10
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int i, j, n,choose1,choose2;
+	int mas[size][size] = { 0 };
+	int MAS[size];
+	int right = 1, down;
+	using std::endl;
+	using std::cout;
+	using std::cin;
+	//заполняем в массиве "прямоугольник" из единиц
+	for (i = 0; i < size - 4; i++)
+		for (j = 0; j < size - 4; j++)
+			mas[i][j] = 1;
+		//вывод массива
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				cout <<  " " << mas[i][j];
+			puts("");
+		}
+		cout << "Куда двигать матрицу? 1-вправо 2-вниз 0-выход" << endl;  cin >> choose1;
+		cout << "Насколько двигать?" << endl; cin >> choose2;
+		int begin = 0; //начало матрицы
+		int position = begin + choose2 - 1; //позиция записи в промежуточный массив
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++) //копируем числа в промежуточный массив
+			{
+				if (position >= size - 1) position = begin;
+				else position++;
+				if (choose1 == right) MAS[position] = mas[j][i];
+				else MAS[position] = mas[i][j];
+			}
+				for (i = 0; i < size; i++) //копируем числа обратно в другом порядке
+					if (choose1 == right) mas[j][i] = MAS[i];
+					else mas[i][j] = MAS[i];
+		}
+	return 0;
+}
