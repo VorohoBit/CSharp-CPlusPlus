db91b1a6d03078f7f1da1bbd21fdd272c2d081d6
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 22:01:03 +0300

    Lab 2 Zadanie 2
    Написать программу, которая определяет, попадает ли точка с заданными координатами в
    область, закрашенную на рисунке серым цветом. Результат работы программы вывести в виде
    текстового сообщения. Параметр R вводится с клавиатуры.

---
+++ C++/Lab 2/Zadanie 2/laba_2(2)(C++).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x,y,r;
+    cout << "Vvedite X(int): "; cin >> x;
+    cout << "Vvedite Y(int): "; cin >> y;
+    cout << "Vvedite R(int): "; cin >> r;
+    if (abs(y) > r || abs(x) > r)
+    {
+        cout << "Так не получится,согласно уравнению окружности с центром в какой-либо точке x^2+y^2=R^2";
+        return 0;
+    }
+    if (r < 1)
+    {
+        cout << "Радиус не может быть отрицательным или раным нулю";
+        return 0;
+    }
+    if (y > abs(x))
+    {
+        cout << "Вы не попали в серую зону";
+    }
+    else if (abs(x)>=abs(y) && abs(x)<=r) cout << "Вы попали в серую зону";
+    else cout << "Ты не попал";
+    return 0;
+}
