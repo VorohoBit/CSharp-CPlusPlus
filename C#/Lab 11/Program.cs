20d0f94a02c5fa0ad7b3e6553f042dc2ff9447be
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-20 00:22:47 +0300

    Lab 11
    В классе В определить индексатор для исходного массива. Вывести в программе на экран элементы массива через индексатор. Добавить в В еще
    один массив и определить индексатор и для него. Вывести на экран значения элементов второго массива через индексатор. Второй массив инициализировать
    при описании (то есть НЕ в конструкторе) . Создать параметризованный класс С со статическим полем. В программе продемонстрировать умение работы со
    статическим полем и параметризацией класса. В качестве параметров взять строковый тип и числовой тип (то есть создать 2 объекта с разными
    параметрами). Статическое поле – тип строка.

---
+++ C#/Lab 11/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace C_sharp__laba11
+{
+    public class class_B
+    {
+        private int[] arr1 = { 1, 2, 3, 4 };
+        private int[] arr2 = { 5, 6, 7, 8 };
+        public int this[int index1, int index2] //индекс
+        {
+            get
+            {
+                if (index1 == -1) { return arr2[index2]; }
+                else if (index2 == -1) { return arr1[index1]; }
+                else { return arr1[index1] + arr2[index2]; }
+
+            }
+            set
+            {
+                if (index1 == -1) { arr2[index2] = value; }
+                else if (index2 == -1) { arr1[index1] = value; }
+            }
+        }
+    }
+
+    public class class_C
+    {
+        public static string str = "No No";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Демонстрация работы со статическим полем и параметризированным классом");
+            Console.WriteLine("1)Выведем изначальное значение str:");
+            Console.WriteLine(class_C.str);
+            Console.WriteLine("2)Добавим ещё одну фразу к изначальному значению str:");
+            class_C.str += " Dont tuch me there";
+            Console.WriteLine(class_C.str);
+            Console.WriteLine("3)И ещё одну)");
+            class_C.str += " this is my no no square";
+            Console.WriteLine(class_C.str);
+            Console.WriteLine("Демонстрация работы с индексатором");
+            class_B B = new class_B();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(B[i, -1] + " ");
+                B[-1, i] += B[i, -1];
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(B[-1, i] + " ");
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(B[i, i] + " ");
+            }
+        }
+    }
+}
