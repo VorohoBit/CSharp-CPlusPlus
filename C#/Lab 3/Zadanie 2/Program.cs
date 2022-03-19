75cfc3c72eae375fdc8055bc016f603ca33b1f5b
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 23:50:45 +0300

    Lab 3 Zadanie 2
    Определить номер N области, в которой находится точка M(x,y) с заданными координатами. Границы области относить к области с наибольшим номером.

---
+++ C#/Lab 3/Zadanie 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace С_sharp__laba3_zadanie2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0;
+            Console.WriteLine("Введите знач. X для Т.M:");
+            double X = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите знач. Y для Т.M:");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            if ((X < 0 && Y < 0) || (X >= 0 && Y >= 0)) 
+            {
+                N = 3;
+            }
+            if ((X < 0 && Y > 0) || (X >= 0 && Y <= 0))
+            {
+                N = 4;
+            }
+            if (X >=0 && Y < 1 - X*X)
+            {
+                N = 2;
+            }
+            if (X < 0 && Y < 1 - X * X)
+            {
+                N = 1;
+            }
+            Console.WriteLine($"Т.M c координатами {(X,Y)} лежит в области N={N}");
+        }
+    }
+}
