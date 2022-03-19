f216a93ea35df89f7386ea43e899b5f3b03b4db9
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 23:08:43 +0300

    Lab 20
    1. Создать шаблон заданного класса. Определить конструкторы, деструктор, перегруженную
    операцию присваивания (“=”) и операции, заданные в варианте задания.
    2. Написать программу тестирования, в которой проверяется использование шаблона для
    стандартных типов данных.
    3. Выполнить тестирование.
    4. Определить пользовательский класс, который будет использоваться в качестве параметра
    шаблона. Определить в классе необходимые функции и перегруженные операции.
    5. Написать программу тестирования, в которой проверяется использование шаблона для
    пользовательского типа.
    6. Выполнить тестирование.
    
    Класс − одномерный массив. Дополнительно перегрузить следующие операции:
    1)«+» − сложение массивов; 2) «[]» − доступ по индексу; 3) «+» − сложить элемент с массивом.

---
+++ C++/Lab 20/laba_20_1(C++).cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+class Myclass
+{
+private:
+    T value;
+public:
+    Myclass(T value)
+    {
+        this->value = value;
+    }
+    void Data()
+    {
+        cout << sizeof
+        (value) << endl;
+    }
+    Myclass<T>& operator+(const Myclass<T>& tmp)
+    {
+        value =value+ tmp.value;
+        return *this;
+    }
+};
+class Point
+{
+private:
+    int x, y, z;
+public:
+    Point()
+    {
+        x = y = z = 0;
+    }
+    Point(int x, int y, int z)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+    Point& operator+(const Point& tmp)
+    {
+        x =x+ tmp.x;
+        y =y+ tmp.x;
+        z =z+ tmp.z;
+        return *this;
+    }
+};
+
+int main()
+{
+    Point a(10,10,10);
+    Myclass<Point> c(a);
+    Myclass<Point> d(a);
+    Myclass<Point> e(a);
+    e = c + d;
+    e.Data();
+    return 0;
+}
+
