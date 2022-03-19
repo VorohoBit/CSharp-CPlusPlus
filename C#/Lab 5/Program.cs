01ff786399c80e5c826e584ca7ab9423ba75a82d
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-20 00:07:11 +0300

    Lab 5
    Для заданного условия составить процедуру и придумать несколько наборов текстовых данных для откладки. Возможно использование как статических массивов, так и динамических. Ввод исходных данных осуществить из файла или с клавиатуры. 
    Написать программу для вычисления среднего арифметического  значения всех элементов заданного массива D. Для отрицательных значений использовать их абсолютные значения.

---
+++ C#/Lab 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace C_sharp__laba5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во элементов массива:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите нижний диапазон:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите верхний диапазон:");
+            int l = Convert.ToInt32(Console.ReadLine());
+            double sum = 0.0;
+            int[] D = new int[n];
+            for (int i=0;i<n;i++)
+            {
+                Random rnd = new Random();
+                D[i] = rnd.Next(m, l);
+                Console.WriteLine($"D[i]={D[i]}");
+                sum += Math.Abs(D[i]);
+            }
+            sum = sum / n;
+            Console.WriteLine($"Среднее арифмитическое знач. всех элементов заданного массива D={sum}");
+        }
+    }
+}
