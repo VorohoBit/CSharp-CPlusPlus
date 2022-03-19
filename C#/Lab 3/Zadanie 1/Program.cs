046a09ea94a65d6619508c6a7a192c851dfc9682
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 23:46:46 +0300

    Lab 3 Zadanie 1
    Вычислить значение функции F. При получении в знаменателя нуля дать соответствующее сообщение.

---
+++ C#/Lab 3/Zadanie 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C_sharp__laba3_zadanie1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double F;
+            double min, max;
+            Console.WriteLine("Введите знач. X:");
+            double X = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите знач. Y:");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите знач. Z:");
+            double Z = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Находим min(x,y+z)");
+            if (X + Z < Y + Z) min = X + Z; else min = Y + Z;
+            Console.WriteLine($"Min(x,y+z)={min}");
+            Console.WriteLine("Находим max(x,y)");
+            if (X > Y) max = X; else max = Y;
+            Console.WriteLine($"Max(x,y)={max}");
+            if(max+Math.Sin(Z)==0) Console.WriteLine("Знаменатель равен 0,а на ноль делить нельзя");
+            else 
+            {
+                Console.WriteLine("Находим знач. выражение F");
+                F = min / (max + Math.Sin(Z));
+                Console.WriteLine($"F={F}");
+            }
+        }
+    }
+}
