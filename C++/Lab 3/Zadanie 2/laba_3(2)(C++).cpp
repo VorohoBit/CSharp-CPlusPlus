d51d65df83e90e2842567192790733d00e8e049e
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 22:07:01 +0300

    Lab 3 Zadanie 2
    Вычислить и вывести на экран в виде таблицы значения функции на интервале от xнач до xкон
    с шагом dx. Таблицу снабдить заголовком и шапкой.

---
+++ C++/Lab 3/Zadanie 2/laba_3(2)(C++).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x1,x2,dx,y;
+    cout << "Vvedite X1(int):"; cin >> x1;
+    cout << "Vvedite X2(int):"; cin >> x2;
+    cout << "Vvedite SHAG(int):"; cin >> dx;
+    if (x2 < x1 || dx<0) cout << "Конечная точка всегда больше начальной и шаг не может быть отрицательынм" << endl;
+    cout << "|-+-+-+-+-|"<<endl;
+    for (int i = x1; i < x2; i += dx)
+    {
+        y = 2 - ((1-i-pow(i,2) / (pow(2*i+pow(i,2),1/2))));
+        cout << "    Y=" << y << endl;
+    }
+    cout << "|+-+-+-+-+|"<<endl;
+    return 0;
+}
