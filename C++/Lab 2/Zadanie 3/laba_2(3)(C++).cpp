3334a238be81405819d5094d09a42a90c3bcc201
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 22:03:10 +0300

    Lab 2 Zadanie 3
    Написать программу, которая по введенному значению аргумента вычисляет значение
    функции, заданной в виде графика. Параметр R вычислить самостоятельно.

---
+++ C++/Lab 2/Zadanie 3/laba_2(3)(C++).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <math.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double x, y;
+    cout << "Введите значение X: "; cin >> x;
+    if (x > -6 && x <= -3) y = x + 3;
+    else if (x <= 0 && x > -3) y = -(pow(x, 2) / 3) + 3;
+    else if (x > 0 && x < 7) y = (-x / 2) + 3;
+    else if (x > 6 && x <= 9) y = x - 6;
+    cout << "Значение Y=" << y;
+    return 0;
+}
