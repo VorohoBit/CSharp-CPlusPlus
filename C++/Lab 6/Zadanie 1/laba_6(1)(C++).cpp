d151e63363a43c530d7739236503e31ac5045f06
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 22:15:24 +0300

    Lab 6 Zadanie 1
    Дана строка. Подсчитать количество содержащихся в ней строчных латинских букв.

---
+++ C++/Lab 6/Zadanie 1/laba_6(1)(C++).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x, k = 0;
+	string str;
+	cout << "Введите строку:"; getline(cin,str);
+	x = size(str);
+	for (int i = 0; i < x; i++)
+	{
+		if ((str[i] > 96) && (str[i] < 123)) k++;
+	}
+	cout << "Кол-во строчных латинских букв:" << k << endl;
+	return 0;
+
+}
