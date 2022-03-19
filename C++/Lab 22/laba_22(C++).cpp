9bbe21688c732fe01f217dd7bfc098fd9d77a998
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 23:16:33 +0300

    Lab 22
    Даны два выражения Z1 и Z2. Написать функции для вычисления этих выражений с
    организацией обнаружения нештатной ситуации (например, деление на ноль) и ее обработки.
    Передача аргументов в функции – по ссылкам.
    В случае успеха значения Z1 и Z2 должны быть приблизительно одинаковыми.

---
+++ C++/Lab 22/laba_22(C++).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+#include <clocale>
+#include <string>
+using namespace std;
+
+double func1(float& x)
+{
+    if (x + sqrt(pow(x, 2) - 4) + 2 == 0) throw (string)"WARNING!Ошибка деления на ноль!";
+    else return (sqrt(2 * sqrt(pow(x, 2) - 4) + 2 * x)) / (x + sqrt(pow(x, 2) - 4) + 2);
+}
+double func2(float& x)
+{
+    if (sqrt(x + 2) == 0) throw(char*)"WARNING!Ошибка деления на ноль!";
+    else return 1 / (sqrt(x + 2));
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float x;
+    cout << "Введите x:"; cin >> x;
+    try
+    {
+        cout << "Результат ф-ции #1:" << func1(x) << endl;
+        cout << "Результат ф-ции #2:" << func2(x) << endl;
+    }
+    catch (string y)
+    {
+        cout << y << endl;
+    }
+    return 0;
+}
