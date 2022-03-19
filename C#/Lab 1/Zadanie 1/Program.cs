e7e2837b28eff5c3239dc32e849de2c8b47914b0
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 23:32:31 +0300

    Lab 1 Zadanie 1
    Напишите процедуру, выводящую сообщение о версии установленной ОС, текущей даты и времени.

---
+++ C#/Lab 1/Zadanie 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C_sharp__laba1_zadanie_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatingSystem os = System.Environment.OSVersion;
+            Console.WriteLine("Операционная система и её версия: {0}",os);
+            System.Console.WriteLine("Текущая дата и время: " + System.DateTime.Now);
+        }
+    }
+}
