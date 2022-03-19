9e71bd2b181df0edb5905eb89df0ba0cbd54f3f8
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 21:58:53 +0300

    Lab 2 zadanie 1
    Записать условие, которое является истинным, когда точка с координатами (х, у) попадает в
    заштрихованные участки плоскости

---
+++ C++/Lab 2/Zadanie 1/laba_2(1)(C++).cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float x, y;
+    cout << "Vvedite x:"; cin >> x;
+    cout << "Vvedite y:"; cin >> y;
+    if (x < 1 || x>4 || y < 2 || y>4)cout << "Ti He PoPal";
+    else if (y < 2 || y>4)cout << "Ti He PoPal";
+    else cout << "Ti PoPal";
+    return 0;
+}
+
