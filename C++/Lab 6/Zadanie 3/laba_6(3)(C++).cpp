a17ad3fedd70ac059c9232606ebe6a80896e6827
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 22:19:52 +0300

    Lab 6 Zadanie 3
    Каждая программа должна непрерывно считывать с клавиатуры строки символов и
    проделывать с ними операции указанные в задании. Прекратить выполнение программы
    необходимо в том случае если введена пустая строка.
    Заменить во введенном предложении каждую первую букву слова на прописную.

---
+++ C++/Lab 6/Zadanie 3/laba_6(3)(C++).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <string>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string str;
+    while (true)
+    {
+        cout << "Введите строку:"; getline(cin, str);
+        for (int i = 0; i < str.size(); i++) //size() – возвращающает для строк кол-во символов
+            if ( str[i - 1] == ' ' || (str[i]>64 && str[i]<80)) str[i] = str[i] - 32;
+        cout << "Полученная строка:" << str << endl;
+        if (size(str) == 0) return 0;
+    }
+    return 0;
+}
