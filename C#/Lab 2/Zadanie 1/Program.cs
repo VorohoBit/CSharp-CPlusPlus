af387e3a6e170294b09a4a837bcf7886ae236bb3
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 23:37:54 +0300

    Lab 2 Zadanie 1
    Составить программу  для ввода в диалоге значений переменных A,I,C,L,Name и форматного вывода на экран монитора введенных переменных

---
+++ C#/Lab 2/Zadanie 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace C_sharp__laba2_zadanie1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 3.125;
+            int I = 6006006;
+            float C = -13.24f;
+            bool L = false;
+            string name = "Maxim";
+            System.Console.WriteLine($"double A ={ A }, float C = { C }, int I = { I }, Имя:{ name }, bool L={ L }");
+            Console.WriteLine("Начинаем форматирование");
+            Console.WriteLine("Введите имя:");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите значение double A:");
+            double A1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение  float C:");
+            float C1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение int I:");
+            I = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение bool L:");
+            L = Convert.ToBoolean(Console.ReadLine());
+            System.Console.WriteLine($"name = { name }, L = { L }");
+            System.Console.WriteLine($"A ={ A1 }, C = { C1 }, I = { I }");
+        }
+    }
+}
