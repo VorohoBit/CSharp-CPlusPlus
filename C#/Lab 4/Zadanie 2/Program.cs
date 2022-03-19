6ffa8a8801da09a59c785f9972bd65667cb18595
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 23:58:17 +0300

    Lab 4 Zadanie 2
    Составить и отладить программу для вычисления значений функции y(x) путем суммирования членов её ряда. Суммирование членов ряда проверить до члена ряда, значение которого не будет превышать 10^-6. Вычислить значений суммы ряда s(x) и непосредственно функцию y(x) при указанных контрольных значениях аргумента x. Сравнить и проанализировать данные.

---
+++ C#/Lab 4/Zadanie 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C_sharp__laba4_zadanie2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0, n = 3; //где k-номер итерации,n-переменная для вычисления знаменателя
+            double chisl, znam;
+            double sum = 0,d = 1,x = 0.5; //s-сумма ряда,d-переменная для вычисления числителя,x-контрольное значение
+            double a; //первый член ряда
+            a = x;
+            while (Math.Abs(a)>=0.001)
+            {
+                sum += a; //сумма ряда на текущей позиции
+                chisl = x*x*x*d;
+                znam = n;
+                a = chisl / znam;
+                d = x*x;
+                n += 2;
+                k++; //счетчик итераций
+                Console.WriteLine($" № Итерации:{k}");
+                Console.WriteLine($"a={a}");
+                Console.WriteLine($"sum={sum}\n");
+            }
+            sum = 2 * sum;
+            double y = Math.Log((1 + x) / (1 - x));
+            Console.WriteLine('\t' + " Результаты:" + '\n' +'\n' + " Вычисленная сумма ряда S =" + sum + '\n' + " Кол-во членов ряда - " + k + '\n' + " Функция ln(1+x/1-x) =" + y + '\n');
+        }
+    }
+}
