2715ea2a935cee4ab9db75be4d8c9f5d95cd6560
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 22:17:39 +0300

    Lab 6 Zadanie 2
    В заданиях, связанных с поиском и заменой подстрок, можно считать, что исходная строка
    не содержит перекрывающихся вхождений требуемых подстрок. Кроме этого, можно также
    считать, что удаление или замена любого вхождения подстроки не приведет к появлению в строке
    новых вхождений данной подстроки.
    Даны строки S, S1 и S2. Заменить в строке S все вхождения строки S1 на строку S2.

---
+++ C++/Lab 6/Zadanie 2/laba_6(2)(C++).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <string>
+#include <regex> //для использования regex_replace
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string s; //строковый тип данных
+    cout << "Введите строку s:"; getline(cin, s); //aaaaaa для ввода данных из потока  cat rat cat
+    string s1;
+    cout << "Введите строку s1:"; getline(cin, s1); //aa   cat
+    string s2;
+    cout << "Введите строку s2:"; getline(cin, s2); //aaaa  bat
+    s = regex_replace(s, regex(s1), s2);
+    /*Заменяет фрагмент строки на несколько равных символов, другую строку или фрагмент другой строки.
+    Из данной строки удаляется count символов, начиная с символа pos, и на их место вставляются новые символы.
+    S.replace(pos, count, T) - вставить содержимое строки T. T может быть объектом класса string или C-строкой.
+    */
+    cout << s << endl; //aaaaaaaaaaaa
+    return 0;
+}
