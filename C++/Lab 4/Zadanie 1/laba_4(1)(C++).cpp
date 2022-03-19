b42b44b7375750289de3c320f4f281e167b607cd
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-19 22:10:49 +0300

    Lab 4 Zadanie 1
    В одномерном массиве, состоящем из n вещественных элементов, вычислить:
    1)количество элементов массива, лежащих в диапазоне от А до В;
    2)сумму элементов массива, расположенных после максимального элемента.
    Упорядочить элементы массива по убыванию модулей.

---
+++ C++/Lab 4/Zadanie 1/laba_4(1)(C++).cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+
+
+ int main()
+{
+	 /*сумму элементов массива, расположенных после максимального элемента.
+	 Упорядочить элементы массива по убыванию модулей.*/
+	setlocale(LC_ALL, "Russian");
+	int a, b, n;
+	int m[100];
+	cout << "Введите кол-во элементов массива:"; cin >> n;
+	cout << "Начальный массив:" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		m[i] = rand() % 50 - 25;
+		cout << " " << m[i];
+	}
+	cout << endl;
+
+	//поиск последнего максимального элемента
+	int max = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (m[max] < m[i]) max = i;
+		cout << "Позиция последнего максимального числа:" << max + 1 << endl;
+		break;
+	}
+
+	//подсчитваем сумму после максимального
+	int sum = 0;
+	for (int i = max + 1; i < n; i++)
+	{
+		sum += m[i];
+		cout << "Сумма чисел после максимального:" << sum << endl;
+	}
+
+	//cортировка массива
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if (abs(m[i]) > abs(m[j]))
+			{
+				int temp = m[i];
+				m[i] = m[j];
+				m[j] = temp;
+			}
+	cout << "Сортированный массив:" << endl;
+	for (int i = 0; i < n; i++) cout << " " << m[i];
+	cout << endl;
+
+	/*количество элементов массива, лежащих в диапазоне от А до В;*/
+	cout << "Введите начальный диапозон:"; cin >> a;
+	cout << "Введите конечный диапазон:"; cin >> b;
+	int k = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if ((m[i] > a)&&(m[i]<b)) k++;
+	}
+	cout << "Кол-во элементов,лежащих в диапазоне от " << a << " до " << b << "равно " << k << endl;
+	return 0;
+}
