449cb1a12957424811963967dd9f7233ff600746
VorohoBit <101928787+VorohoBit@users.noreply.github.com>
2022-03-20 00:17:12 +0300

    Lab 8
    В рамках консольного приложения создать класс А с полями а и b и свойством с. Свойство – значение выражения над полями а и b (выражение и
    типы полей – см. вариант в таблице 1). Поля инициализировать при объявлении класса. Конструктор оставить по умолчанию. Проследить, чтобы поля а и b
    напрямую в других классах были недоступны. Создать класс Programm с одним методом – точкой входа. В теле метода создать объект класса А, вывести на
    экран значение свойства с.
    
    Тип полей a b, Выражения
    decimal ++(префиксный),-

---
+++ C#/Lab 8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C_sharp__laba8
+{
+    public class class_A
+    {
+        private decimal a=9.45M, b=3.29M;
+        public (decimal,decimal,decimal,decimal) Property_C
+        {
+            get {return (a - b,b-a,++a,++b);}
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class_A A = new class_A();
+            Console.WriteLine(A.Property_C);
+        }
+    }
+}
